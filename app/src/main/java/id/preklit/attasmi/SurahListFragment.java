110c1564390bc3a61467b7cf60f557c6b27c8fb5
faldyif <faldy.if@gmail.com>
2017-02-10 20:09:18 +0700

    [ADD] Prototype listview

--- app/src/main/java/id/preklit/attasmi/SurahListFragment.java
+++ app/src/main/java/id/preklit/attasmi/SurahListFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -26,6 +28,9 @@ public class SurahListFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    String[] surahNames = {"Android","IPhone","WindowsMobile","Blackberry",
+            "WebOS","Ubuntu","Windows7","Max OS X"};
 
     private OnFragmentInteractionListener mListener;
 
@@ -64,7 +69,14 @@ public class SurahListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_surah_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_surah_list, container, false);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.activity_listview_surah, R.id.label, surahNames);
+
+        ListView listView = (ListView) view.findViewById(R.id.surah_list);
+        listView.setAdapter(adapter);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
